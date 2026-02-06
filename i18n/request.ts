414f53c37ca891e2da5952d56841065e1b3c50e3
ssavior <tmdwn12512@gmail.com>
2026-02-06 17:35:18 +0900

    i18n

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,12 +1,10 @@
 import {getRequestConfig} from 'next-intl/server';
 
-export default getRequestConfig(async ({locale}) => {
-  if (!locale) {
-    locale = 'ko'; // Default to Korean
-  }
+export default getRequestConfig(async ({requestLocale}) => {
+  const locale = (await requestLocale) || 'ko';
   return {
     messages: (await import(`@/messages/${locale}.json`)).default,
     locale,
-    timeZone: 'Asia/Seoul' // Add default timezone to prevent markup mismatches
+    timeZone: 'Asia/Seoul'
   };
 }); 
